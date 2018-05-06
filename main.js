63b1434b040c4ccfccdaae4c691e13522134de50
Tyler Krszjzaniek <t.krszjzaniek@gmail.com>
2018-05-06 17:06:23 -0500

    initialize fizzbuzz

---
+++ main.js
@@ -0,0 +1,26 @@
+var numEntry = function numEntry(){
+    var x = document.getElementById( "num" ).value;
+
+    document.getElementById( "numberdisplay" ).innerHTML = x;
+
+    if( x % 3 === 0 ){
+        document.getElementById( "fizzbuzz" ).innerHTML = "Fizz";
+    }
+
+    else if( x % 5 === 0 ){
+        document.getElementById( "fizzbuzz" ).innerHTML = "Buzz";
+    }
+
+    else if(  x % 3 === 0  &&  x % 5 === 0  ){
+        document.getElementById( "fizzbuzz" ).innerHTML = "Fizzbuzz";
+    }
+
+    else{
+        ( document.getElementById( "fizzbuzz" ).innerHTML = x );
+    }
+};
+
+
+// if the number is divisible by 3, print fizz
+// if the number is divisible by 5, print Buzz
+// if the number is divisible by 3 and 5, print FIZZBUZZ
